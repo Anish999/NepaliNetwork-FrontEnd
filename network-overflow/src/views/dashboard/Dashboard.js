02bcf6bd951d54d31dd1c1b2e7e46059ddd33d3f
Avashism <avash.mishra@selu.edu>
2020-05-05 16:42:27 -0500

    dashboard update

--- network-overflow/src/views/dashboard/Dashboard.js
+++ network-overflow/src/views/dashboard/Dashboard.js
@@ -3,6 +3,8 @@ import MultiItemCarousel from "./components/MultiItemCarousel";
 import { makeStyles } from "@material-ui/core/styles";
 import HomePageNavbar from "../dashboard/components/navbar";
 import CitySelect from "../dashboard/components/locationSearch";
+import jwt_decoded from "jwt-decode";
+import axios from "axios";
 
 const profileBasedEvents = [
   {
@@ -61,68 +63,6 @@ const profileBasedEvents = [
   },
   {
     title: "Events",
-    img: "./event.jpg",
-    description:
-      "We Help Each Other ! Thousands of jobs offered by your own locals",
-  },
-];
-const profileBasedRooms = [
-  {
-    title: "Rooms",
-    img: "./event.jpg",
-    description:
-      "We Help Each Other ! Thousands of jobs offered by your own locals",
-  },
-  {
-    title: "Rooms",
-    img: "./event.jpg",
-    description:
-      "We Help Each Other ! Thousands of jobs offered by your own locals",
-  },
-  {
-    title: "Rooms",
-    img: "./event.jpg",
-    description:
-      "We Help Each Other ! Thousands of jobs offered by your own locals",
-  },
-  {
-    title: "Rooms",
-    img: "./event.jpg",
-    description:
-      "We Help Each Other ! Thousands of jobs offered by your own locals",
-  },
-  {
-    title: "Rooms",
-    img: "./event.jpg",
-    description:
-      "We Help Each Other ! Thousands of jobs offered by your own locals",
-  },
-  {
-    title: "Rooms",
-    img: "./event.jpg",
-    description:
-      "We Help Each Other ! Thousands of jobs offered by your own locals",
-  },
-  {
-    title: "Rooms",
-    img: "./event.jpg",
-    description:
-      "We Help Each Other ! Thousands of jobs offered by your own locals",
-  },
-  {
-    title: "Rooms",
-    img: "./event.jpg",
-    description:
-      "We Help Each Other ! Thousands of jobs offered by your own locals",
-  },
-  {
-    title: "Rooms",
-    img: "./event.jpg",
-    description:
-      "We Help Each Other ! Thousands of jobs offered by your own locals",
-  },
-  {
-    title: "Rooms",
     img: "./event.jpg",
     description:
       "We Help Each Other ! Thousands of jobs offered by your own locals",
@@ -269,6 +209,83 @@ const useStyles = makeStyles({
 });
 
 const Dashboard = () => {
+  var isPetLover;
+  const [profileBasedRooms, updateProfileBasedRooms] = React.useState([]);
+
+  const token = localStorage.getItem("token");
+  const getUserProfile = async () => {
+    if (token !== null) {
+      const decoded = jwt_decoded(token);
+
+      const data = decoded.email;
+
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        await axios
+          .get(`http://localhost:5000/api/users/${data}`, config)
+          .then((res) => {
+            const user = res.data.user;
+            isPetLover = user.hasPet;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } catch (error) {
+        console.log(error.response);
+      }
+    }
+  };
+  const getRoommates = async () => {
+    let importedRooms = [];
+    try {
+      const config = {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      };
+      await axios
+        .get(`http://localhost:5000/api/roommates`, config)
+        .then((response) => {
+          console.log(response.data);
+          const imported = response.data.rommmates;
+          let importedRoom;
+          // console.log(imported);
+          imported.map((r) => {
+            if (r.petsAllowed == isPetLover) {
+              importedRoom = {
+                listingType: r.listingType,
+                address: r.addressId,
+                description: r.description,
+                user: r.userId,
+                image: r.image,
+                contactNumber: r.contactNumber,
+                petsAllowed: r.petsAllowed,
+                datePosted: r.datePosted,
+              };
+              //  console.log(importedRoom);
+              importedRooms.push(importedRoom);
+            }
+          });
+          //  console.log(importedRooms);
+          if (importedRooms.length > profileBasedRooms.length) {
+            updateProfileBasedRooms(importedRooms);
+            importedRooms = [];
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+  getUserProfile();
+  getRoommates();
   const classes = useStyles();
   return (
     <div
@@ -286,22 +303,22 @@ const Dashboard = () => {
       <div style={{ marginLeft: "70px" }}>
         <h3 className={classes.header}>Rooms</h3>
       </div>
-      <MultiItemCarousel cardInfo={profileBasedRooms} />
+      <MultiItemCarousel cardInfo={profileBasedRooms} isRoom={true} />
       <div style={{ marginLeft: "70px" }}>
         <h3 className={classes.header}>Events</h3>
       </div>
-      <MultiItemCarousel cardInfo={profileBasedEvents} />
+      <MultiItemCarousel cardInfo={profileBasedEvents} isRoom={false} />
       <div style={{ textAlign: "center" }}>
         <span className={classes.header}>Most Saved Topics</span>
       </div>
       <div style={{ marginLeft: "70px" }}>
         <h3 className={classes.header}>Top Rooms</h3>
       </div>
-      <MultiItemCarousel cardInfo={topEvents} />
+      <MultiItemCarousel cardInfo={topEvents} isRoom={false} />
       <div style={{ marginLeft: "70px" }}>
         <h3 className={classes.header}>Top Events</h3>
       </div>
-      <MultiItemCarousel cardInfo={topRooms} />
+      <MultiItemCarousel cardInfo={topRooms} isRoom={false} />
     </div>
   );
 };
--- network-overflow/src/views/dashboard/components/MultiItemCarousel.js
+++ network-overflow/src/views/dashboard/components/MultiItemCarousel.js
@@ -2,37 +2,40 @@ import React, { Component, useState } from "react";
 import Carousel from "react-multi-carousel";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "./Card";
+import RCard from "../../../shared/card-template/RCard";
 import "react-multi-carousel/lib/styles.css";
-const MultiItemCarousel = props => {
+const MultiItemCarousel = (props) => {
   console.log(props);
+
+  const isRoom = props.isRoom;
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 5,
-      slidesToSlide: 3 // optional, default to 1.
+      slidesToSlide: 3, // optional, default to 1.
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 2,
-      slidesToSlide: 2 // optional, default to 1.
+      slidesToSlide: 2, // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
       items: 1,
-      slidesToSlide: 1 // optional, default to 1.
-    }
+      slidesToSlide: 1, // optional, default to 1.
+    },
   };
   const useStyles = makeStyles({
     carouselOverflow: {
-      overflow: "visible"
+      overflow: "visible",
     },
 
     cardContainer: {
       display: "flex" /* or inline-flex */,
       flexDirection: "row",
       width: "100%",
-      justifyContent: "center"
-    }
+      justifyContent: "center",
+    },
   });
   const classes = useStyles();
 
@@ -50,10 +53,10 @@ const MultiItemCarousel = props => {
         keyBoardControl={true}
         transitionDuration={500}
       >
-        {props.cardInfo.map(c => (
+        {props.cardInfo.map((c) => (
           <div>
             <div className={classes.cardContainer}>
-              <Card title={c.title} description={c.description} />
+              {isRoom ? <RCard roommate={c} /> : <Card roommate={c} />}
             </div>
           </div>
         ))}
